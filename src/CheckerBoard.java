84169011e2adc3d7c18fd30ee3a9f89cbde57a0e
tiffanywatson <tiffanywatson@ip-168-27-85-73.columbusstate.edu>
2017-11-08 12:40:45 -0500

    First Commit

---
+++ src/CheckerBoard.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class CheckerBoard{
+	public static int rows = 8;
+	public static int columns = 8;
+	public static Color col1 = Color.LIGHT_GRAY;
+	public static Color col2 = Color.PINK;
+	
+	public static void main(String[] args) {
+		JFrame checkerBoard = new JFrame();
+		checkerBoard.setSize(800, 800);
+		checkerBoard.setTitle("CheckerBoard");
+		Container pane = checkerBoard.getContentPane();
+		pane.setLayout(new GridLayout(rows, columns));
+		Color temp;
+		for (int i = 0; i < rows; i++) {
+			if (i % 2 == 0) {
+				temp = col1;
+			}
+			else {
+				temp = col2;
+			}
+			for (int j = 0; j < columns; j++) {
+				JPanel panel = new JPanel();
+				panel.setBackground(temp);
+				if (temp.equals(col1)) {
+					temp = col2;
+				}
+				else {
+					temp = col1;
+				}
+				pane.add(panel);
+			}
+		}
+        checkerBoard.setVisible(true);
+
+	}
+
+}
