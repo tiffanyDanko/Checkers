4a72fb12f9cd31709efbd9583fde35edeacc6f29
fusmith283 <33499717+fusmith283@users.noreply.github.com>
2017-11-09 15:22:01 -0500

    Create Piece_Interface.java

---
+++ src/Piece_Interface.java
@@ -0,0 +1,12 @@
+
+public interface Piece_Interface {
+	
+	void setName(String name);
+	void setColor(String color);
+	void setCoordinate(String coordinate);
+	
+	String getName();
+	String getColor();
+	String getCoordinate();
+
+}
